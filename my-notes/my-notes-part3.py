250f191f65b99d03e73daaab60c18af333914b50
chaxiaoli001 <50093760+chaxiaoli001@users.noreply.github.com>
2019-11-19 17:46:09 +0800

    Part.3.B.2.classes-2.ipynb-1
    Part.3.B.2.classes-2.ipynb-1

---
+++ my-notes/my-notes-part3.py
@@ -0,0 +1,73 @@
+Part.3.A.conquering-difficulties
+1.战胜难点
+	a.类，以及面向对象编程（Class，OOP）
+	b.迭代器、生成器、装饰器（Iterators、Generators、Decorators）
+	c.正则表达式（Regular Expressions）
+	d.巴科斯-诺尔范式（Backus Normal Form）
+1.1
+读不懂也要读完，读完之后再读很多遍
+不断自己动手归纳总结整理
+1.2
+知识就是知识，它没有任何义务去具备幽默生动的属性；
+手艺就是手艺，它没有任何义务去具备有趣欢乐的属性。
+1.3 耐心学习
+首先，平静地接受了它枯燥的本质；其次，就是经过多次实践已然明白，无论多枯燥，总能读完；无论多难，多读几遍总能读懂
+
+Part.3.B.1.classes-1.ipynb
+1 类 —— 面向对象编程
+1.1 面向对象编程
+	a.本节内容，不专属于哪个编程语言（比如 Python、JavaScript 或者 Golang）。
+	面向对象编程（Object Oriented Programming, OOP）是一种编程的范式（Paradigm），或者说，是一种方法论（Methodology）
+1.2 基本术语
+	a.面向对象编程（OOP），是使用对象（Objects）作为核心的编程方式。进而就可以把对象（Objects）的数据和运算过程封装（Encapsulate）在内部，
+		而外部仅能根据事先设计好的界面（Interface）与之沟通。
+	b.在程序设计过程中，需要对标现实世界创造对象——最直接手段就是 抽象（Abstract）。
+	c.从用编程语言创造对象的角度去看，所谓的界面，就由这两样东西构成：
+		属性（Attributes） —— 用自然语言描述，通常是名词（Nouns）。 “必要的特征”，叫做对象的属性
+		方法（Methods） —— 用自然语言描述，通常是动词（Verbs）。 “必要的行为”，叫做对象的方法。
+	d.程序里创建对象，做法常是：
+		先创建最抽象的类（Class），然后再创建子类（Subclass）
+		它们之间是从属关系是：Class ⊃ Subclass，在 OOP 中，这叫继承（Inheritance）关系，Subclass继承故拥有Class的所有属性
+		创建好一个类之后，可以根据它创建它的许多个实例（Instances）。
+	e.小结
+		对象，封装，抽象
+		界面，属性，方法
+		继承，类，子类，实例
+1.3 对象和类的理解
+	你创造了一个类（Class），这时候你是创作者，从你眼里望过去，那就是个类（Class）；
+	而后你根据这个类的定义，创建了很多实例（Instances）；
+	接下来一旦你开始使用这些实例的时候，你就成了使用者，从使用者角度望过去，手里正在操作的，就是各种对象（Objects）
+
+Part.3.B.2.classes-2.ipynb
+1 类 —— Python 的实现
+1.1 Defining Class
+	定义：Class 使用 class 关键字
+	注：与函数定义不同的地方在于，Class 接收参数不是在 class Classname(): 的括号里完成
+
+
+Part.3.B.3.decorator-iterator-generator.ipynb
+
+
+Part.3.B.4.regex.ipynb
+
+
+Part.3.B.5.bnf-ebnf-pebnf.ipynb
+
+
+Part.3.C.breaking-good-and-bad.ipynb
+
+
+Part.3.D.indispensable-illusion.ipynb
+
+
+Part.3.E.to-be-thorough.ipynb
+
+
+Part.3.F.social-selfteaching.ipynb
+
+
+Part.3.G.the-golden-age-and-google.ipynb
+
+
+Part.3.H.prevent-focus-drifting.ipynb
+
